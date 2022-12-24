1425dc0027493c1e108fa65815112732a54b7528
mattmeyerink <meyerink@umich.edu>
2022-12-23 16:38:03 -0800

    Initial Commit

---
+++ CRUMBS/CRUMBSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRUMBSApp.swift
+//  CRUMBS
+//
+//  Created by Matthew Meyerink on 12/23/22.
+//
+
+import SwiftUI
+
+@main
+struct CRUMBSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
