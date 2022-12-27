636923ec81f8a85b29a0e049a4f41713a90d3719
mattmeyerink <meyerink@umich.edu>
2022-12-26 17:51:03 -0800

    add icons to navigation bar

--- CRUMBS/NavigationBar/NavigationBar.swift
+++ CRUMBS/NavigationBar/NavigationBar.swift
@@ -12,11 +12,43 @@ struct NavigationBar: View {
         VStack {
             ZStack {
                 Color(red: 0, green: 0.75, blue: 1)
-                Text("Hello From the Navigation Bar!")
+                
+                HStack {
+                    VStack {
+                        Image(systemName: "button.programmable")
+                        
+                        Text("Buttons")
+                    }
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Image(systemName: "globe.americas.fill")
+                        
+                        Text("Map")
+                    }
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Image(systemName: "list.bullet.rectangle")
+                        
+                        Text("List")
+                    }
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Image(systemName: "chart.pie.fill")
+                        
+                        Text("Stats")
+                    }
+                }
+                .padding(.horizontal, 30)
             }
                 .edgesIgnoringSafeArea(.all)
             
         }
-        .frame(height: 50)
+            .frame(height: 60)
     }
 }
