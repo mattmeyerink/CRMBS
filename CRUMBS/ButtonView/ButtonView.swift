ac6452a4d59e4daef4f5e2e58acfe123b75232b9
mattmeyerink <meyerink@umich.edu>
2022-12-31 13:32:16 -0500

    add base button components

--- CRUMBS/ButtonView/ButtonView.swift
+++ CRUMBS/ButtonView/ButtonView.swift
@@ -9,6 +9,60 @@ import SwiftUI
 
 struct ButtonView: View {
     var body: some View {
-        Text("Hello From the Button View!")
+        VStack {
+            Text("CRMBS")
+                .font(.system(size: 30, weight: .bold))
+                .foregroundColor(.white)
+                .padding(.bottom, 10)
+            
+            Spacer()
+            
+            HStack {
+                Button(action: dropPin) {
+                    VStack {
+                        Image(systemName: "fork.knife")
+                        
+                        Text("Food")
+                    }
+                }
+                .buttonStyle(FoodButton())
+                
+                
+                Button(action: dropPin) {
+                    VStack {
+                        Image(systemName: "bag.fill")
+                        
+                        Text("Shopping")
+                    }
+                }
+                .buttonStyle(ShoppingButton())
+            }
+            
+            HStack {
+                Button(action: dropPin) {
+                    VStack {
+                        Image(systemName: "leaf.fill")
+                        
+                        Text("Nature")
+                    }
+                }
+                .buttonStyle(NatureButton())
+                
+                Button(action: dropPin) {
+                    VStack {
+                        Image(systemName: "popcorn.fill")
+                        
+                        Text("Entertainment")
+                    }
+                }
+                .buttonStyle(EntertainmentButton())
+            }
+            
+            Spacer()
+        }
+    }
+    
+    func dropPin() -> Void {
+        print("Dropped a pin")
     }
 }
---
+++ CRUMBS/ButtonView/PinButtonStyles.swift
@@ -0,0 +1,65 @@
+//
+//  PinButtonStyles.swift
+//  CRUMBS
+//
+//  Created by Matthew Meyerink on 12/31/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct FoodButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.system(size: 25, weight: .bold))
+            .padding(.vertical, 15)
+            .padding(.horizontal, 20)
+            .foregroundColor(Color(red: 0.1, green: 0.1, blue: 0.1))
+            .background(Color.orange)
+            .clipShape(Capsule())
+            .scaleEffect(configuration.isPressed ? 0.85 : 1)
+            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+    }
+}
+
+struct NatureButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.system(size: 25, weight: .bold))
+            .padding(.vertical, 15)
+            .padding(.horizontal, 20)
+            .foregroundColor(Color(red: 0.1, green: 0.1, blue: 0.1))
+            .background(Color.mint)
+            .clipShape(Capsule())
+            .scaleEffect(configuration.isPressed ? 0.85 : 1)
+            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+    }
+}
+
+struct ShoppingButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.system(size: 25, weight: .bold))
+            .padding(.vertical, 15)
+            .padding(.horizontal, 20)
+            .foregroundColor(Color(red: 0.1, green: 0.1, blue: 0.1))
+            .background(Color.cyan)
+            .clipShape(Capsule())
+            .scaleEffect(configuration.isPressed ? 0.85 : 1)
+            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+    }
+}
+
+struct EntertainmentButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.system(size: 25, weight: .bold))
+            .padding(.vertical, 15)
+            .padding(.horizontal, 20)
+            .foregroundColor(Color(red: 0.1, green: 0.1, blue: 0.1))
+            .background(Color.red)
+            .clipShape(Capsule())
+            .scaleEffect(configuration.isPressed ? 0.85 : 1)
+            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+    }
+}
