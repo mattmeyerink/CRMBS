03dda94766c46039e7c8a5e54367eba6b369f15e
mattmeyerink <meyerink@umich.edu>
2023-01-29 17:37:10 -0500

    default map view to user's current location

--- CRUMBS/MapView/MapView.swift
+++ CRUMBS/MapView/MapView.swift
@@ -9,10 +9,22 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    @StateObject private var locationManager = LocationManager()
 
+    var region: Binding<MKCoordinateRegion>? {
+        guard let location = locationManager.location else {
+            return MKCoordinateRegion.goldenGateRegion().getBinding()
+        }
+        
+        let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 500, longitudinalMeters: 500)
+        
+        return region.getBinding()
+    }
+    
     var body: some View {
-        Map(coordinateRegion: $region, showsUserLocation: true, userTrackingMode: .constant(.follow))
-            .edgesIgnoringSafeArea(.top)
+        if let region = region {
+            Map(coordinateRegion: region, showsUserLocation: true, userTrackingMode: .constant(.follow))
+                .edgesIgnoringSafeArea(.top)
+        }
     }
 }
